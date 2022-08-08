a9d2ca29049218c3a9311278a2b5b447e9367264
CJ Wheelan <cjwheelan@gmail.com>
2022-08-08 14:28:22 -0400

    Initial Commit

---
+++ opencv-practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opencv-practice
+//
+//  Created by CJ Wheelan on 8/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
